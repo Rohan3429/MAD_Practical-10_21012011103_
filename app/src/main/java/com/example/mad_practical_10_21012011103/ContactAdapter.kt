b71e7f32aacdc97b919177d8feaff63e3c506f34
student <student@gnu.ac.in>
2023-10-21 10:28:00 +0530

    21/10/2023

--- app/src/main/java/com/example/mad_practical_10_21012011103/ContactAdapter.kt
+++ app/src/main/java/com/example/mad_practical_10_21012011103/ContactAdapter.kt
@@ -5,11 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 
-class ContactAdapter(val context:Context,val array:ArrayList<Contact> ):ArrayAdapter<Contact>(context,0,array){
+class ContactAdapter(context:Context,val array:ArrayList<Contact> ):ArrayAdapter<Contact>(context,0,array){
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val view=LayoutInflater.from(parent.context).inflate(R.layout.data_item,parent)
+        val view=LayoutInflater.from(parent.context).inflate(R.layout.data_item,parent,false)
+        view.findViewById<TextView>(R.id.name1).text=array.get(position).name
+        view.findViewById<TextView>(R.id.no1).text=array.get(position).phoneno
+        view.findViewById<TextView>(R.id.email1).text=array.get(position).emailid
+        view.findViewById<TextView>(R.id.address1).text=array.get(position).address
+
         return view
-        return super.getView(position, convertView, parent)
     }
 }
--- app/src/main/java/com/example/mad_practical_10_21012011103/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_10_21012011103/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.mad_practical_10_21012011103
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val fav1:FloatingActionButton=findViewById(R.id.fav1)
+        fav1.setOnClickListener {
+            SendDataToListview()
+            Intent(this,MapsActivity::class.java).apply { startActivity(this) }
+        }
     }
+    fun SendDataToListview() {
+        val personlistview = findViewById<ListView>(R.id.listview1)
+        val personlist = arrayListOf<Contact>(
+            Contact("1", "Rohan", "rohan@gmail.com", "9265248766", "deesa", 24.2585, 72.1907),
+            Contact("2", "Rushi", "rushi@gmail.com", "9724050500", "bhavnagar", 21.7645, 72.1519),
+            Contact("3", "Vinit", "vinit@gmail.com", "9975641266", "surat", 21.1702, 72.8311),
+            Contact("4", "Raj", "raj@gmail.com", "9898546799", "ransipur", 23.7448, 72.7851),
+            Contact("5", "Smit", "smit@gmail.com", "8152084599", "gondal", 21.9612, 70.7939)
+        )
+        personlistview.adapter = ContactAdapter(this, personlist)
+    }
+
 }
